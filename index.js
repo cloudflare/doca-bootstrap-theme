c33f195e8244d67f78715ac2983b4bf721b29ea8
tajo <vojtech@miksu.cz>
2016-07-13 12:24:38 -0700

    README: how to create your own theme

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 
 module.exports = {
   App: require('./build/app'),
+  Constraints: require('./build/constraints'),
+  Definition: require('./build/definition'),
+  Endpoint: require('./build/endpoint'),
+  ExampleObject: require('./build/exampleObject'),
   Head: require('./build/head'),
+  ObjectDefinitionTable: require('./build/objectDefinitionTable'),
+  Schema: require('./build/schema'),
+  Sidebar: require('./build/sidebar')
 };
