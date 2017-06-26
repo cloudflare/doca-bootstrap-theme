748f156898e4c1fb4baf8b9ed4b5fe22f80d022c
Nathan Mills <nathanm@instructure.com>
2017-06-26 07:31:11 -0600

    add mediaType section

---
+++ components/mediaType.js
@@ -0,0 +1,85 @@
+const React = require('react');
+const ObjectDefinitionTable = require('./objectDefinitionTable');
+const MarkdownPreview = require('react-marked-markdown').MarkdownPreview;
+const ImmutablePropTypes = require('react-immutable-proptypes');
+const Component = require('react-pure-render/component');
+const ExampleObject = require('./exampleObject');
+
+class MediaType extends Component {
+
+  static propTypes = {
+    schema: ImmutablePropTypes.map.isRequired,
+  };
+
+  state = {
+    showDefinition: false,
+  };
+
+  handleToggle = () => {
+    this.setState(prevState => ({
+      showDefinition: !prevState.showDefinition,
+    }));
+  };
+
+  render() {
+    const { schema } = this.props;
+    const { showDefinition } = this.state;
+    return (
+      <article className="panel panel-primary">
+        <div className="panel-heading">
+          <div id={schema.get('html_id')} />
+          <h2>{schema.get('mediaType')}</h2>
+        </div>
+        <div className="panel-body">
+          <h3>{schema.get('description')}</h3>
+          {schema.get('extended_description') &&
+          <MarkdownPreview value={schema.get('extended_description')} />}
+
+          <header id={`${schema.get('html_id')}-properties`}>
+            {IS_JAVASCRIPT &&
+            <p>
+              <a onClick={this.handleToggle} className="btn btn-info">
+                <span>{showDefinition ? 'Hide' : 'Show'}</span>{' '}
+                properties and constraints defined on the object
+              </a>
+            </p>
+            }
+          </header>
+
+          {(showDefinition || !IS_JAVASCRIPT) &&
+          <div>
+            {schema.getIn(['object_definition', 'objects']).count() ?
+              <div>
+                {schema.getIn(['object_definition', 'objects']).valueSeq().map(obj =>
+                  <div key={obj.get('title')}>
+                    {obj.get('title') &&
+                    <div>
+                      <h4>{obj.get('title')}</h4>
+                    </div>
+                    }
+                    {obj.get('example') && <ExampleObject example={obj.get('example')} />}
+                    <ObjectDefinitionTable definitions={obj.get('all_props')} />
+                  </div>
+                )}
+              </div>
+              :
+              <div>
+                {schema.getIn(['object_definition', 'example']) &&
+                <ExampleObject example={schema.getIn(['object_definition', 'example'])} />
+                }
+
+                <ObjectDefinitionTable
+                  definitions={schema.getIn(['object_definition', 'all_props'])}
+                />
+              </div>
+            }
+          </div>
+          }
+        </div>
+      </article>
+    );
+  }
+
+}
+
+module.exports = MediaType;
